6fa2a4af15c695995fd26a048f2da3675ba34fca
Alpha Liu <alpha_liu@sva-arcadyan.com.cn>
2012-03-20 20:58:09 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = 'b30589edbb5edeae5621fdb2f9323213c5807b9d45e421e3f4f651456f3b75520cd5206e2d3229fa28f541060db8bf1a5d423ef098b82a855ab9f7acdba53893'
