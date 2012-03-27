fa97315384ffb7395707a9be4ef46ad00a675464
Alpha Liu <alpha.liu@me.com>
2012-03-27 17:22:47 +0800

    depot_d

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,10 @@
 class Product < ActiveRecord::Base
   default_scope :order => 'title'
   
+  has_many :line_items
+  
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   #validate stuff...
   validates :title, :image_url, :description, :presence => true
   validates :price, :numericality => { :greater_than_or_equal_to => 0.01 }
@@ -11,4 +15,12 @@ class Product < ActiveRecord::Base
   }
   validates :title, :length => { :minimum => 10 }
 
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base, 'Line Items present')
+      return false
+    end
+  end
 end
