46020e67d5e992eaadb528a77e67db4e0eadb250
Alpha Liu <alpha_liu@sva-arcadyan.com.cn>
2012-03-29 23:05:03 +0800

    depot_f half-finished

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,5 +2,6 @@ class StoreController < ApplicationController
   def index
     @products = Product.all
     #@count = increment_counter
+    @cart = current_cart
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def hidden_div_if(condition, attributes = {}, &block) 
+    if condition 
+      attributes["style"] = "display: none" 
+    else
+      attributes["style"] = "" 
+    end 
+    content_tag("div", attributes, &block) 
+  end
+
 end
