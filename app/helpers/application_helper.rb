45e27d977d838aa8fd965709248c2d0f0a74e93e
Alpha Liu <alpha_liu@sva-arcadyan.com.cn>
2012-03-30 23:16:54 +0800

    depot_f

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,6 @@ module ApplicationHelper
   def hidden_div_if(condition, attributes = {}, &block) 
     if condition 
       attributes["style"] = "display: none" 
-    else
-      attributes["style"] = "" 
     end 
     content_tag("div", attributes, &block) 
   end
